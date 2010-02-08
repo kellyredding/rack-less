a1607c4a1ae32cd18169ee007db195e04ad598e3
Kelly <kelly@kelredd.com>
2010-02-07 21:09:04 -0600

    forgot to add this file into the last commit

---
+++ test/unit/request_test.rb
@@ -0,0 +1,68 @@
+require "#{File.dirname(__FILE__)}/../test_helper"
+require 'rack/less/request'
+
+class RequestTest < Test::Unit::TestCase
+
+  context 'Rack::Less::Request' do
+    setup do 
+      @defaults = Rack::Less::Base.defaults
+    end
+    
+    context "basic object" do
+      should "have some attributes" do
+        [ :request_method,
+          :path_info,
+          :path_resource_name,
+          :path_resource_format,
+          :source,
+          :for_css?,
+          :for_less?
+        ].each do |a|
+          assert_respond_to less_request("GET", "/foo.css"), a, "request does not respond to #{a.inspect}"
+        end   
+      end
+      
+      should "know it's resource name" do
+        assert_equal 'foo', less_request("GET", "/foo.css").path_resource_name
+        assert_equal 'bar', less_request("GET", "/foo/bar.css").path_resource_name
+      end
+      
+      should "know it's resource format" do
+        assert_equal 'css', less_request("GET", "/foo.css").path_resource_format
+        assert_equal 'css', less_request("GET", "/foo/bar.css").path_resource_format
+      end
+    end
+    
+    should_not_be_a_valid_rack_less_request({
+      :method      => "POST",
+      :resource    => "/foo.html",
+      :description => "a non-css resource"
+    })
+
+    should_not_be_a_valid_rack_less_request({
+      :method      => "POST",
+      :resource    => "/foo.css",
+      :description => "a css resource"
+    })
+    
+    should_not_be_a_valid_rack_less_request({
+      :method      => "GET",
+      :resource    => "/foo.html",
+      :description => "a non-css resource"
+    })
+    
+    should_not_be_a_valid_rack_less_request({
+      :method      => "GET",
+      :resource    => "/foo.css",
+      :description => "a css resource hosted somewhere other than where Rack::Less expects them"
+    })
+
+    should_be_a_valid_rack_less_request({
+      :method      => "GET",
+      :resource    => "/stylesheets/foo.css",
+      :description => "a css resource hosted where Rack::Less expects them"
+    })
+
+  end
+
+end
