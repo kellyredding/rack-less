54e59a204de4dd17bd621709979b2b5991046d07
Kelly <kelly@kelredd.com>
2011-08-25 22:27:39 -0500

    updated the gem and test env; moving tests to assert framework

--- rack-less.gemspec
+++ rack-less.gemspec
@@ -10,7 +10,7 @@ Gem::Specification.new do |s|
   s.email       = ["kelly@kelredd.com"]
   s.homepage    = "http://github.com/kelredd/rack-less"
   s.summary     = %q{LESS CSS preprocessing for Rack apps.}
-  s.description = %q{Use rack middleware to handle LESS CSS preprocessing.  Processing happens on requests to LESS resources.  This allows you to develop, check in, and deploy unprocessed LESS stylesheets and leave the processing to the middleware.  Allows for optimizing by environment: never-cache, always-reprocess in development; cache, process-once in production (for example).}
+  s.description = %q{LESS CSS preprocessing for Rack apps.}
 
   s.files         = `git ls-files`.split("\n")
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
@@ -18,7 +18,8 @@ Gem::Specification.new do |s|
   s.require_paths = ["lib"]
 
   s.add_development_dependency("bundler", ["~> 1.0"])
-  s.add_development_dependency("test-belt", ["= 0.2.1"]) # locked to a specific version for test stability
+  s.add_development_dependency("assert", ["~>0.3.0"])
+
   s.add_development_dependency("sinatra", [">= 0.9.4"])
   s.add_development_dependency("rack-test", [">= 0.5.3"])
   s.add_development_dependency("webrat", [">= 0.6.0"])
--- test/helper.rb
+++ test/helper.rb
@@ -1,11 +1,10 @@
-require 'rubygems'
-require 'bundler'
-Bundler.setup
+# this file is automatically required in when you require 'assert' in your tests
+# put test helpers here
 
-require 'test_belt'
-require 'test/env'
+# add root dir to the load path
+$LOAD_PATH.unshift(File.expand_path("../..", __FILE__))
 
-class Test::Unit::TestCase
+class Assert::Context
 
   def file_path(*segments)
     segs = segments.unshift([File.dirname(__FILE__), '..']).flatten
@@ -13,17 +12,15 @@ class Test::Unit::TestCase
   end
 
   def self.should_compile_source(name, desc)
-    context desc do
-      setup do
-        @compiled = File.read(File.join(@source_folder, "#{name}_compiled.css"))
-        @source = Rack::Less::Source.new(name, :folder => @source_folder)
-      end
+    setup do
+      @compiled = File.read(File.join(@source_folder, "#{name}_compiled.css"))
+      @source = Rack::Less::Source.new(name, :folder => @source_folder)
+    end
 
-      should "compile LESS" do
-        assert_equal @compiled.strip, @source.compiled.strip, '.compiled is incorrect'
-        assert_equal @compiled.strip, @source.to_css.strip, '.to_css is incorrect'
-        assert_equal @compiled.strip, @source.css.strip, '.css is incorrect'
-      end
+    should "compile LESS" do
+      assert_equal @compiled.strip, @source.compiled.strip, '.compiled is incorrect'
+      assert_equal @compiled.strip, @source.to_css.strip, '.to_css is incorrect'
+      assert_equal @compiled.strip, @source.css.strip, '.css is incorrect'
     end
   end
 
@@ -45,32 +42,31 @@ class Test::Unit::TestCase
   end
 
   def self.should_not_be_a_valid_rack_less_request(args)
-    context "to #{args[:method].upcase} #{args[:resource]} (#{args[:description]})" do
-      setup do
-        @request = less_request(args[:method], args[:resource])
-      end
+    desc "to #{args[:method].upcase} #{args[:resource]} (#{args[:description]})"
+    setup do
+      @request = less_request(args[:method], args[:resource])
+    end
 
-      should "not be a valid endpoint for Rack::Less" do
-        not_valid = !@request.get?
-        not_valid ||= !@request.for_css?
-        not_valid ||= @request.source.files.empty?
-        assert not_valid, 'request is a GET for .css format and has source'
-        assert !@request.for_less?, 'the request is for less'
-      end
+    should "not be a valid endpoint for Rack::Less" do
+      not_valid = !@request.get?
+      not_valid ||= !@request.for_css?
+      not_valid ||= @request.source.files.empty?
+      assert not_valid, 'request is a GET for .css format and has source'
+      assert !@request.for_less?, 'the request is for less'
     end
   end
-  def self.should_be_a_valid_rack_less_request(args)
-    context "to #{args[:method].upcase} #{args[:resource]} (#{args[:description]})" do
-      setup do
-        @request = less_request(args[:method], args[:resource])
-      end
 
-      should "be a valid endpoint for Rack::Less" do
-        assert @request.get?, 'the request is not a GET'
-        assert @request.for_css?, 'the request is not for css'
-        assert !@request.source.files.empty?, 'the request resource has no source'
-        assert @request.for_less?, 'the request is not for less'
-      end
+  def self.should_be_a_valid_rack_less_request(args)
+    desc "to #{args[:method].upcase} #{args[:resource]} (#{args[:description]})"
+    setup do
+      @request = less_request(args[:method], args[:resource])
+    end
+
+    should "be a valid endpoint for Rack::Less" do
+      assert @request.get?, 'the request is not a GET'
+      assert @request.for_css?, 'the request is not for css'
+      assert !@request.source.files.empty?, 'the request resource has no source'
+      assert @request.for_less?, 'the request is not for less'
     end
   end
 
